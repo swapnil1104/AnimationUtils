c2047efd86eb1ccc232fc8904280a3b3d4616efd
swapnil1104 <swapniltiwari775@gmail.com>
2019-06-03 13:42:02 +0530

    added library in app module

--- app/src/main/java/com/broooapps/animutils/MainActivity.java
+++ app/src/main/java/com/broooapps/animutils/MainActivity.java
@@ -1,7 +1,11 @@
 package com.broooapps.animutils;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.broooapps.animationutils.AnimUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final View v = findViewById(R.id.view);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AnimUtil.collapseLayoutHorizontally(v, 200);
+                    }
+                });
+            }
+        }, 2000);
     }
 }
